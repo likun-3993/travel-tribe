1012b19d631526faea31f83b296ede7cbe139b54
likun-3993 <76272089+likun-3993@users.noreply.github.com>
2022-07-29 22:09:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require("express");
 require("dotenv").config();
 var cors = require("cors");
 const app = express();
-const user = require("./router/user");
+//const user = require("./router/user");
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/user", user);
+//app.use("/api/user", user);
 
 app.get("/", (req, res) => res.send("running"));
 
