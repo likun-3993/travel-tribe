d5d1f2a7ee6c5ecb9797252586975518563af214
likun-3993 <76272089+likun-3993@users.noreply.github.com>
2022-08-12 20:40:34 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb+srv://user:user@cluster0.hsvp2.mongodb.net/?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb://user:user@cluster0-shard-00-00.hsvp2.mongodb.net:27017,cluster0-shard-00-01.hsvp2.mongodb.net:27017,cluster0-shard-00-02.hsvp2.mongodb.net:27017/?ssl=true&replicaSet=atlas-gwlfgd-shard-0&authSource=admin&retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("db connected"))
   .catch((err) => console.log(err));
